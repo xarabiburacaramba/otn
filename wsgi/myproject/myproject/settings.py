0a5f987f2e5b0016dbfcbcc385eb52e818612b9e
Dzmitry Kozhukh <zmicjer@gmail.com>
2016-09-01 14:39:43 +0200

    test3

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -28,7 +28,7 @@ SECRETS = secrets.getter(os.path.join(DATA_DIR, 'secrets.json'))
 SECRET_KEY = SECRETS['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = 'True'
 
 from socket import gethostname
 ALLOWED_HOSTS = [
--- wsgi/myproject/myproject/urls.py
+++ wsgi/myproject/myproject/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponse
+
+def index(request):
+  return HttpResponse('Hello World')
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
