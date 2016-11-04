d8fa34241cb94037a86cc2cf7a6932198b86f94a
Dzmitry Kozhukh <zmicjer@gmail.com>
2016-11-04 15:14:48 +0100

    test1104_3

--- wsgi/myproject/myproject/views.py
+++ wsgi/myproject/myproject/views.py
@@ -7,10 +7,15 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 import os
+import io
 import sys
+import logging
 import psycopg2
 import mimetypes
 import json
+import zipfile
+
+import StringIO
 
 import myproject.settings
 
@@ -53,3 +58,65 @@ def get_folder_date(request):
     folder_path = os.path.join(myproject.settings.STATIC_ROOT, 'helpapp')
     time_modified = {'time':('%s' % os.path.getmtime(folder_path) )}
     return JsonResponse(time_modified, safe=False)
+    
+def get_folder_content(request):
+    filename = 'content.zip'
+    chunk_size = 8192
+    response = StreamingHttpResponse(FileWrapper(open(zip_paths(os.path.join(myproject.settings.STATIC_ROOT, 'helpapp')),'rb'),chunk_size),content_type='application/zip')
+    response['Content-Disposition'] = 'attachment; filename=%s' % filename
+    return response
+           
+def zip_paths(paths):
+    """
+    Compresses directories and files to a single zip file.
+    
+    Returns the zip file as a data stream, None if error.
+    """
+    # Check single path vs. multiple
+    if isinstance(paths, str):
+        paths = (paths,)
+
+    # Filter out non-existent paths
+    paths = [x for x in paths if os.path.exists(x)]
+
+    # Make sure the zip file will actually contain something
+    if not paths:
+        logging.warning("No files/folders to add, not creating zip file")
+        return None
+
+    logging.debug("Creating zip file")
+    zip_stream = io.BytesIO()
+    try:
+        zfile = zipfile.ZipFile(zip_stream, 'w', compression=zipfile.ZIP_DEFLATED)
+    except EnvironmentError as e:
+        logging.warning("Couldn't create zip file")
+        return None
+
+    for path in paths:
+        if os.path.isdir(path):
+            root_len = len(os.path.abspath(path))
+
+            # If compressing multiple things, preserve the top-level folder names
+            if len(paths) > 1:
+                root_len -= len(os.path.basename(path)) + len(os.sep)
+
+            # Walk through the directory, adding all files
+            for root, dirs, files in os.walk(path):
+                archive_root = os.path.abspath(root)[root_len:]
+                for f in files:
+                    fullpath = os.path.join(root, f)
+                    archive_name = os.path.join(archive_root, f)
+                    try:
+                        zfile.write(fullpath, archive_name, zipfile.ZIP_DEFLATED)
+                    except EnvironmentError as e:
+                        logging.warning("Couldn't add file: %s", (str(e),))
+        else:
+            # Exists and not a directory, assume a file
+            try:
+                zfile.write(path, os.path.basename(path), zipfile.ZIP_DEFLATED)
+            except EnvironmentError as e:
+                logging.warning("Couldn't add file: %s", (str(e),))
+    zfile.close()
+    zip_stream.seek(0)
+
+    return zip_stream
