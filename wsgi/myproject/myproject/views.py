c7bb2fab2caab998a046935835714d556a5acc59
Dzmitry Kozhukh <zmicjer@gmail.com>
2016-11-25 13:49:24 +0100

    test1125_1

--- wsgi/myproject/myproject/views.py
+++ wsgi/myproject/myproject/views.py
@@ -20,46 +20,85 @@ import myproject.settings
 
 
 def index(request):
-  return HttpResponse('Hello World')
+  return HttpResponse(' ')
   
 connection=psycopg2.connect("dbname=otn user=adminphaewqf password=dSeAnnfTss-R host=127.7.14.2 port=5432")
 
+
 def get_compositions(request):
-    cursor=connection.cursor()
-    cursor.execute("select array_to_json(array_agg(map_compositions)) from map_compositions")
-    c=cursor.fetchone()[0]
-    return JsonResponse(c, safe=False) 
-    cursor.close()
+    if request.GET.get('language')=='cz':
+      cursor=connection.cursor()
+      cursor.execute("select array_to_json(array_agg(t)) from (select composition_id, name__cz as name, description__cz as description, custom_attributes__cz as custom_attributes from map_compositions) t")
+      c=cursor.fetchone()[0]
+      return JsonResponse(c, safe=False) 
+      cursor.close()
+    else:
+      cursor=connection.cursor()
+      cursor.execute("select array_to_json(array_agg(t)) from (select composition_id, name, description, custom_attributes from map_compositions) t")
+      c=cursor.fetchone()[0]
+      return JsonResponse(c, safe=False) 
+      cursor.close()
     
     
 def get_layers(request, composition_id):
-    cursor=connection.cursor()
-    cursor.execute("SELECT array_to_json(array_agg(t)) FROM (select a.*, b.layer_order from map_layers a, compositions_layers b where a.layer_id=b.layer_id and b.composition_id='%s' order by layer_order asc) t" %composition_id)
-    d=cursor.fetchone()[0]
-    return JsonResponse(d, safe=False) 
-    cursor.close()
-    
+    if request.GET.get('language')=='cz':
+      cursor=connection.cursor()
+      cursor.execute("SELECT array_to_json(array_agg(t)) FROM (select a.*, b.layer_order from (select layer_id, name__cz as name, description__cz as description, wms_url__cz as wms_url, wms_layer_name__cz as wms_layer_name, custom_attributes__cz as custom_attributes from map_layers) a, compositions_layers b where a.layer_id=b.layer_id and b.composition_id='%s' order by layer_order asc) t" %composition_id)
+      d=cursor.fetchone()[0]
+      return JsonResponse(d, safe=False) 
+      cursor.close()
+    else:
+      cursor=connection.cursor()
+      cursor.execute("SELECT array_to_json(array_agg(t)) FROM (select a.*, b.layer_order from (select layer_id, name, description, wms_url, wms_layer_name, custom_attributes from map_layers) a, compositions_layers b where a.layer_id=b.layer_id and b.composition_id='%s' order by layer_order asc) t" %composition_id)
+      d=cursor.fetchone()[0]
+      return JsonResponse(d, safe=False) 
+      cursor.close()
+           
 def get_news(request, time_period):
-    cursor=connection.cursor()
-    cursor.execute("SELECT array_to_json(array_agg(t)) FROM (SELECT * from news where the_time between (now() - interval '%s hours') and now() order by the_time desc) t" %time_period)
-    e=cursor.fetchone()[0]
-    return JsonResponse(e, safe=False) 
-    cursor.close()
+    if request.GET.get('language')=='cz':
+      cursor=connection.cursor()
+      cursor.execute("SELECT array_to_json(array_agg(t)) FROM (SELECT news_id, category, the_time, geom, title__cz as title, content__cz as content, custom_attributes__cz as custom_attributes from news where the_time between (now() - interval '%s hours') and now() order by the_time desc) t" %time_period)
+      e=cursor.fetchone()[0]
+      return JsonResponse(e, safe=False) 
+      cursor.close()
+    else:
+      cursor=connection.cursor()
+      cursor.execute("SELECT array_to_json(array_agg(t)) FROM (SELECT news_id, category, the_time, geom, title, content, custom_attributes from news where the_time between (now() - interval '%s hours') and now() order by the_time desc) t" %time_period)
+      e=cursor.fetchone()[0]
+      return JsonResponse(e, safe=False) 
+      cursor.close()
     
 def get_all_news(request):
-    cursor=connection.cursor()
-    cursor.execute("SELECT array_to_json(array_agg(t)) FROM (SELECT * from news order by the_time desc) t")
-    f=cursor.fetchone()[0]
-    return JsonResponse(f, safe=False) 
-    cursor.close()
+    if request.GET.get('language')=='cz':
+      cursor=connection.cursor()
+      cursor.execute("SELECT array_to_json(array_agg(t)) FROM (SELECT news_id, category, the_time, geom, title__cz as title, content__cz as content, custom_attributes__cz as custom_attributes from news order by the_time desc) t")
+      f=cursor.fetchone()[0]
+      return JsonResponse(f, safe=False) 
+      cursor.close()
+    else:
+      cursor=connection.cursor()
+      cursor.execute("SELECT array_to_json(array_agg(t)) FROM (SELECT news_id, category, the_time, geom, title, content, custom_attributes from news order by the_time desc) t")
+      f=cursor.fetchone()[0]
+      return JsonResponse(f, safe=False) 
+      cursor.close()
     
 def get_folder_date(request):
     folder_path = os.path.join(myproject.settings.STATIC_ROOT, 'helpapp')
     time_modified = {'time':('%s' % os.path.getmtime(folder_path) )}
     return JsonResponse(time_modified, safe=False)
     
+def get_folder_date__cz(request):
+    folder_path = os.path.join(myproject.settings.STATIC_ROOT, 'helpapp_cz')
+    time_modified = {'time':('%s' % os.path.getmtime(folder_path) )}
+    return JsonResponse(time_modified, safe=False)
+    
 def get_folder_content(request):  
     response = HttpResponse(zip_paths(os.path.join(myproject.settings.STATIC_ROOT, 'helpapp')).getvalue(), content_type='application/zip')
+    response['Content-Disposition'] = 'attachment; filename={}'.format("%s" % ('content.zip'))
+    return response
+
+def get_folder_content__cz(request):  
+    response = HttpResponse(zip_paths(os.path.join(myproject.settings.STATIC_ROOT, 'helpapp_cz')).getvalue(), content_type='application/zip')
     response['Content-Disposition'] = 'attachment; filename={}'.format("%s" % ('content.zip'))
     return response
            
